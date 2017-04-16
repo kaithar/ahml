b98cac4c3e8e2be4205a02bf5661318678e7d156
Kaithar <kaithar@users.noreply.github.com>
2017-04-16 21:47:49 +0100

    Only test with 1 element to make the test consistent

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,10 +1,10 @@
 
 def test_json(ply_test_helper):
 	state = ply_test_helper(
-		'''\json{"a": {"b": "c","d": 8}}''',
+		'''\json{"a": {"b": "c"}}''',
 		[
-			('JSON','{"a": {"b": "c","d": 8}}')
+			('JSON','{"a": {"b": "c"}}')
 		],
-		r'''("ahml_document", [<ahml_JSON "OrderedDict([('a', {'b': 'c', 'd': 8})])">])''',
+		r'''("ahml_document", [<ahml_JSON "OrderedDict([('a', {'b': 'c'})])">])''',
 		'')
-	assert state['variables']['a'] == {"b": "c","d": 8}
+	assert state['variables']['a'] == {"b": "c"}
